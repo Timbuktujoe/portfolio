db4e917bc2e8e7a51995d8d71a8b63b6396b46b8
Timbuktujoe <23214407+Timbuktujoe@users.noreply.github.com>
2022-02-12 14:52:02 +0100

    added game

---
+++ src/Components/Breakout.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+import Board from "./breakout/Board"
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            <Board />
+        </>
+    )
+}
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export default function Navbar() {
+    return (
+        <>
+            <nav className="navbar">
+                <Link to="/">Home</Link>
+                <Link to="/about">About Me</Link>
+                <Link to="/breakout">Breakout</Link>
+            </nav>
+        </>
+    )
+}
